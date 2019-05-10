9b27c3e8ffdaa43ebe732973f30121c59eb40e4e
Thad Bryson <thadbry@gmail.com>
2019-05-10 13:03:26 -0400

    project:init command, created classes for other commands

--- bin/flysystem-sync.php
+++ bin/flysystem-sync.php
@@ -11,10 +11,10 @@ require_once __DIR__ . '/../vendor/autoload.php';
 use Illuminate\Console\Application;
 use Illuminate\Container\Container;
 use Illuminate\Events\Dispatcher;
+use TCB\Flysystem\Commands;
 
 $app = new Application(new Container, new Dispatcher, 'v1.0');
 $app->setName('Flysystem Sync CLI');
 
-$app->add(new \TCB\Flysystem\Commands\Init\InitConfig);
-
+$app->add(new Commands\Project\Init);
 $app->run();
